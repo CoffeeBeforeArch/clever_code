5e49b5061581e1ece755e3a8cf614636cb8fcd99
Nick <CoffeeBeforeArch@gmail.com>
2019-09-17 14:43:36 -0400

    add clang-format style file and find_odd example

---
+++ find_odd/find_odd.cpp
@@ -0,0 +1,87 @@
+// This program shows a clever way to find which number appears an odd
+// number of times in a vector (assumes there is only 1 that fits this
+// criteria).
+// By: Nick from CoffeeBeforeArch
+
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+using std::accumulate;
+using std::bit_xor;
+using std::cout;
+using std::endl;
+using std::vector;
+
+// Bad implementation of findOdd
+// Compares every element against every other element
+// Earliest it can exit is after one full scan of the vector
+int findOddBad(vector<int> v) {
+  // For each element in the vector
+  for (auto i : v) {
+    // Compare against all other vector elements
+    int tmp = 0;
+    for (auto j : v) {
+      // Increment if we found an instance of "i"
+      if (j == i) {
+        tmp++;
+      }
+    }
+    // If it appeared an odd number of times, we found the answer
+    if (tmp % 2) {
+      return i;
+    }
+  }
+
+  // Returns 0 otherwise
+  return 0;
+}
+
+// Always completes 1 full scan of the array
+// Much better than the previous, but still a little "old-fashioned"
+int findOddBetter(vector<int> v) {
+  // Accumulate partial results in tmp
+  int tmp = 0;
+
+  // Perform bit-wise XOR all elements together
+  // Background:
+  //  1.) XOR is an associative operation
+  //  2.) n XOR n = 0 (any number XOR itself is 0)
+  //  3.) n XOR 0 = n (any number XOR 0 is itself)
+  // Explanation:
+  //  The XOR of all even-count elements together is 0 because of (2).
+  //  The XOR of all odd-count elements together is "n" because of (2)
+  //  and (3).
+  //  Performing this operations can occur in any order because of (1),
+  //  so we can just intuitively XOR from the beginning to the end.
+  for (auto i : v) {
+    tmp ^= i;
+  }
+
+  return tmp;
+}
+
+// How about 1 line of code!
+// Performs the same operation as the above, but we're making use of
+// the STL!
+int findOddBest(vector<int> v) {
+  return accumulate(begin(v), end(v), 0, bit_xor<int>());
+}
+
+int main() {
+  // Create a vector
+  vector<int> v1 = {2, 2, 3, 4, 4, 1, 1, 3, 4};
+
+  // Get 3 solutions using our 3 functions
+  int sol1 = findOddBad(v1);
+  int sol2 = findOddBetter(v1);
+  int sol3 = findOddBest(v1);
+
+  // Print out the results
+  cout << "sol1 == " << sol1 << endl;
+  cout << "sol2 == " << sol2 << endl;
+  cout << "sol3 == " << sol3 << endl;
+
+  return 0;
+}
