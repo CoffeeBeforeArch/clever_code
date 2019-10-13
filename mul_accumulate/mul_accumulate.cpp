02006cde8e70716dcfe0eb0f6838ef3eee9def0a
Nick <CoffeeBeforeArch@gmail.com>
2019-10-13 16:53:20 -0400

    add multiple and accumulate example

---
+++ mul_accumulate/mul_accumulate.cpp
@@ -0,0 +1,43 @@
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
+// Naive implementation of mulAndAccumulate
+// Uses our own hand-written for-loop
+int badMulAccumulate(vector<int> &v1, vector<int> &v2) {
+  int tmp = 0;
+  for (auto i = 0u; i < v1.size(); i++) {
+    tmp += v1[i] * v2[i];
+  }
+
+  return tmp;
+}
+
+// Better implementation using std::inner_product!
+// Allows us to traverse two containers together without indexing!
+// Reduces our implementation to a single line of code.
+// By default, performs multiplication and addition
+int goodMulAccumulate(vector<int> &v1, vector<int> v2) {
+  // inner_product(begin(v1), end(v1), begin(v2), 0, plus<int>(),
+  // multiplies<int>());
+  return inner_product(begin(v1), end(v1), begin(v2), 0);
+}
+
+// Now we have an implementation we can parallelize and vectorize!
+int goodMulAccumulate17(vector<int> &v1, vector<int> &v2){
+  return transform_reduce(begin(v1), end(v1), begin(v2), 0, plus<int>(), multiplies<int>());
+}
+
+// Add parallelize and vectorize
+int main() {
+  vector<int> v1 = {1, 2, 3, 4, 5};
+  vector<int> v2 = {10, 100, 1000, 10000, 100000};
+
+  cout << badMulAccumulate(v1, v2) << endl;
+  cout << goodMulAccumulate(v1, v2) << endl;
+
+  return 0;
+}
