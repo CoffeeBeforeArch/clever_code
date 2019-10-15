b84cefa8714a5163459112478196c182cc342839
Nick <CoffeeBeforeArch@gmail.com>
2019-10-15 12:08:16 -0400

    Add quicksort example finalized

--- quicksort/quicksort.cpp
+++ quicksort/quicksort.cpp
@@ -9,6 +9,7 @@
 using std::cout;
 using std::distance;
 using std::endl;
+using std::generate;
 using std::next;
 using std::partition;
 using std::swap;
@@ -16,7 +17,7 @@ using std::vector;
 
 // Create a sub-array for elements "<=" and ">" the pivot
 // Takes an array, left index, and pivot index as arguments
-int partition(vector<int> v, int l, int p) {
+int partition(vector<int> &v, int l, int p) {
   // Start with a sub-array that is empty
   int low = l - 1;
 
@@ -44,7 +45,7 @@ int partition(vector<int> v, int l, int p) {
 // Recursive function that partitions the array into "<="/">"
 // sub-arrays, and calls quicksort on them
 // Classic implementation (very much C-style)
-void naiveQuicksort(vector<int> v, int l, int p) {
+void naiveQuicksort(vector<int> &v, int l, int p) {
   // Recursively called until only a single element left
   if (l < p) {
     // Partition the array into "<="/">" sub-arrays
@@ -67,17 +68,59 @@ void goodQuicksort(vector<int>::iterator first, vector<int>::iterator last) {
   }
 
   // Take the pivot as the middle element
-  auto pivot = *std::next(first, distance(first, last) / 2);
+  auto pivot = *next(first, distance(first, last) / 2);
 
   // Partition to get all elements less that the pivot
-  auto mid_1 = partition(first, last, [pivot](auto &em) { return em < pivot; });
+  auto mid_1 =
+      partition(first, last, [pivot](const auto &em) { return em < pivot; });
 
-  // Partition to get all elements greater than the pivot 
-  auto mid_2 = partition(mid_1, last, [pivot](auto &em) { return pivot >= em; });
+  // Partition to get all elements greater than the pivot
+  auto mid_2 =
+      partition(mid_1, last, [pivot](const auto &em) { return !(pivot < em); });
 
-  // Recursively call quicksort
+  // Recusive calls to quicksort until we are left with a single element
   goodQuicksort(first, mid_1);
   goodQuicksort(mid_2, last);
 }
 
-int main() { return 0; }
+int main() {
+  // Create two vectors of random ints (copy one to the other)
+  vector<int> v1;
+  v1.resize(10);
+  generate(begin(v1), end(v1), []() { return rand() % 100; });
+  vector<int> v2 = v1;
+
+  // Print vectors before
+  cout << "BEFORE" << endl;
+  cout << "v1: ";
+  for (auto i : v1) {
+    cout << i << " ";
+  }
+  cout << endl;
+
+  cout << "v2: ";
+  for (auto i : v2) {
+    cout << i << " ";
+  }
+  cout << endl;
+
+  // Sort the two vectors
+  naiveQuicksort(v1, 0, v1.size() - 1);
+  goodQuicksort(begin(v2), end(v2));
+
+  // Print the sorted arrays
+  cout << "AFTER" << endl;
+  cout << "v1: ";
+  for (auto i : v1) {
+    cout << i << " ";
+  }
+  cout << endl;
+
+  cout << "v2: ";
+  for (auto i : v2) {
+    cout << i << " ";
+  }
+  cout << endl;
+
+  return 0;
+}
